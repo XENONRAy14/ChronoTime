bf5b03d966f52f59c95a47a0772c1e92d41973bb
Rayan Belhocine <rayanbelho@hotmail.com>
2025-04-14 04:08:07 +0200

    Ajout de l'attribut isAdmin dans les informations utilisateur renvoyées lors de la connexion

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -83,7 +83,8 @@ router.post('/login', async (req, res) => {
         id: user._id,
         username: user.username,
         email: user.email,
-        name: user.name
+        name: user.name,
+        isAdmin: user.isAdmin || false
       }
     });
   } catch (error) {
