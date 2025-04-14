bf3c63becd8157d904f40574ebeb8448435e3613
Rayan Belhocine <rayanbelho@hotmail.com>
2025-04-14 17:51:39 +0200

    FIX: Ajout de l'import mongoose manquant dans admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const User = require('../models/User');
 const Chrono = require('../models/Chrono');
 const adminAuth = require('../middleware/admin');
